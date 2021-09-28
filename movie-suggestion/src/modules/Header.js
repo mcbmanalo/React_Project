cc1467440c90b389810c5a6f8282b36f894155c8
mcbmanalo <xtremex471@gmail.com>
2021-09-29 00:42:26 +0800

    Made a Header component for the website that will contain the routing of different pages

---
+++ movie-suggestion/src/modules/Header.js
@@ -0,0 +1,18 @@
+
+const Header = () => {
+  return (
+    <div className='App-header'>
+      <div className='App-image'>
+        <label>Image</label>
+      </div>
+
+      <div className='App-pages'>
+        <p>Home</p>
+        <p>Movies</p>
+        <p>TV Series</p>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
