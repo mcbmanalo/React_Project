f74d32765a7d34dd893c8702ab6fcfb558e7aa7e
mcbmanalo <xtremex471@gmail.com>
2021-09-29 00:45:47 +0800

    Implemented the different components into the default page

--- movie-suggestion/src/App.js
+++ movie-suggestion/src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './modules/Header';
+import GenreOptions from './modules/GenreOptions';
+import Generator from './modules/Generator';
+import { useState } from 'react';
+
+
+const genres = ['Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'History', 'Horror', 'Music', 'Mystery']
 
 function App() {
+  const [selectedGenres, setSelectedGenres] = useState([])
+
+  const addSelectedGenres = (genre) => {
+    if (selectedGenres.includes(genre)) return
+    let newSelectedGenres = [...selectedGenres]
+    newSelectedGenres.push(genre)
+    console.log(newSelectedGenres)
+    setSelectedGenres(newSelectedGenres)
+  }
+
+  const generateMovieTV = () => {
+    console.log('Add code for choosing random movie')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      
+      <Generator generateMovieTV={generateMovieTV} />
+
+      <GenreOptions addSeletectedGenre={addSelectedGenres} genres={genres} />
     </div>
   );
 }
