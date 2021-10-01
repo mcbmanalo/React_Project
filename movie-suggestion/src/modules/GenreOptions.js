4ef76a781dfde3cd5a8dd5831623909652d038a7
mcbmanalo <xtremex471@gmail.com>
2021-10-01 10:51:13 +0800

    Added additional states and separated genres for movie and tv series

--- movie-suggestion/src/modules/GenreOptions.js
+++ movie-suggestion/src/modules/GenreOptions.js
@@ -1,21 +1,45 @@
-import React, {useContext} from 'react';
+import React, {useState, useContext} from 'react';
 import { UserContext } from '../App'
 
 const GenreOptions = () => {
-
   const user = useContext(UserContext);
 
-  const generateGenres = user.genres.map((genre) => {
+  const generateMovieGenres = user.movieGenre.map((genre) => {
     const key = `${genre.name}-${genre.id}`
     return <div onClick={(event) => user.addSelectedGenre(event.target.textContent)} key={key}>{genre.name}</div>
   })
 
+  const generateTVGenres = user.tvGenre.map((genre) => {
+    const key = `${genre.name}-${genre.id}`
+    return <div onClick={(event) => user.addSelectedGenre(event.target.textContent)} key={key}>{genre.name}</div>
+  })
+
+  const setMovieGenre = () => {
+    if (!user.isMovie) user.resetSelectedGenres([])
+    user.setMovieOption(true)
+  }
+
+  const setTVGenre = () => {
+    if (user.isMovie) user.resetSelectedGenres([])
+    user.setMovieOption(false)
+  }
+
   return (
     <div className='App-genres'>
-      <div>Choose a genre that you would like to base the generator</div>
-      <div className='Genres'>
-        {generateGenres}
+      <div className='App-options'>
+        <div onClick={setMovieGenre}>
+          Movie Genres
+        </div>
+        <div onClick={setTVGenre}>
+          TV Series Genres
+        </div>
       </div>
+
+      <div>Choose a genre that you would like to base the generator</div>
+      { user.isMovie ? 
+        <div className='Genres'>{generateMovieGenres}</div> : 
+        <div className='Genres'>{generateTVGenres}</div>
+      }
     </div>
   )
 }
