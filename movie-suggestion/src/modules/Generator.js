3608a379fa9f40e7f9f6aa267fbe75706f77f302
mcbmanalo <xtremex471@gmail.com>
2021-10-03 03:17:37 +0800

    Extracted neccessary props from UserContext

--- movie-suggestion/src/modules/Generator.js
+++ movie-suggestion/src/modules/Generator.js
@@ -3,30 +3,31 @@ import { UserContext } from '../App'
 
 const Generator = () => {
   const user = useContext(UserContext);
+  const {selectedGenres, generateMovieTV, isMaxGenre} = user
   const [isSelectedGenreEmpty, setIsSelectedGenre] = useState(true)
 
   const setSelectedGenres = () => {
-    const selectedGenres = user.selectedGenres.map(genre => genre.name)
+    const userSelectedGenres = selectedGenres.map(genre => genre.name)
     return (
-      <div className='selectedGenres'>Selected Genres: {selectedGenres.join(', ')}</div>
+      <div className='selectedGenres'>Selected Genres: {userSelectedGenres.join(', ')}</div>
     )
   }
 
   const checkSelectedGenre = () => {
-    if (user.selectedGenres.length > 0) return setIsSelectedGenre(false)
+    if (selectedGenres.length > 0) return setIsSelectedGenre(false)
     return setIsSelectedGenre(true)
   }
 
   useEffect(() => {
     checkSelectedGenre()
-  }, [user.selectedGenres])
+  }, [selectedGenres])
 
   return (
     <div className='App-generator'>
-      <div className='quotes' onClick={user.generateMovieTV}>
+      <div className='quotes' onClick={generateMovieTV}>
         Surprise yourself!
         {isSelectedGenreEmpty ? <div></div> : setSelectedGenres()}
-        { user.isMaxGenre ? <div className='error'>Reached max number of allowable genres. (max 3)</div> :
+        { isMaxGenre ? <div className='error'>Reached max number of allowable genres. (max 3)</div> :
           <div></div>
         }
       </div>
