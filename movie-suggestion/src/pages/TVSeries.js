40ee5fef94a7f5b4c5dbadda65c9b6bcd0e28b40
mcbmanalo <xtremex471@gmail.com>
2021-10-02 13:23:03 +0800

    Now loads the api from the TVSeries page and added Loading component to display when API is still loading

--- movie-suggestion/src/pages/TVSeries.js
+++ movie-suggestion/src/pages/TVSeries.js
@@ -1,18 +1,29 @@
 import Header from "../modules/Header";
 import React, {useState, useContext, useEffect} from 'react';
-import { MovieContext, UserContext } from "../App";
+import { UserContext } from "../App";
 import Pagination from "../modules/Pagination";
+import Loading from "../modules/Loading";
 
 const imagepath = process.env.REACT_APP_POSTER_PATH
+const discoverTV = process.env.REACT_APP_TV_LIST 
 const key = process.env.REACT_APP_API_KEY
 
-
 const TVSeries = () => {
-  const [currentPage, setCurrentPage] = useState(0) // this will be used for pagination later
-  const tvSeries = useContext(MovieContext);
+  const [tvList, setNewTVList] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1) // this will be used for pagination later
+  const [lastPage, setLastPage] = useState(1)
   const user = useContext(UserContext);
   const {tvGenre:genres} = user;
-  const {tvList, setNewTVList} = tvSeries
+
+  const getTVList = async() => {
+    const tvListResponse = await fetch(discoverTV)
+    const jsonTVList = await tvListResponse.json()
+    setNewTVList(jsonTVList.results)
+    setCurrentPage(jsonTVList.page)
+    setLastPage(jsonTVList.total_pages)
+    setLoading(false)
+  }
 
   const getGenres = (genreIDs) => {
     let movieGenre = []
@@ -23,6 +34,7 @@ const TVSeries = () => {
   }
 
   const setPagination = (pageNumber) => {
+    setLoading(true)
     setCurrentPage(pageNumber)
     generateNewTVPage(pageNumber)
   }
@@ -31,34 +43,48 @@ const TVSeries = () => {
     let discoverTVLink = `https://api.themoviedb.org/3/discover/tv?api_key=${key}&language=en-US&sort_by=popularity.desc&page=${page}`
     const newTVList = await fetch(discoverTVLink)
     const newTVListResult = await newTVList.json()
-    setNewTVList(newTVListResult)
+    setNewTVList(newTVListResult.results)
+    setLoading(false)
   }
 
-  const generateTVSeries = tvList.results.map((series) => {
-    return (
-      <div key={series.id} className='movie'>
-        <img className='image' alt={series.name} src={imagepath + series.poster_path}/>
-        <div className='movieContent'>
-          <div>{series.name}</div> 
-          <div>{getGenres(series.genre_ids)}</div>
-          <div>First Air Date: {series.first_air_date}</div>
-          <div>{series.overview}</div>
-        </div>
-      </div>
-    )
-  })
+  const generateTVShows = () => {
+    if (tvList.length > 0) {
+      return (
+        tvList.map((series) => {
+          return (
+            <div key={series.id} className='movie'>
+              <img className='image' alt={series.name} src={imagepath + series.poster_path}/>
+              <div className='movieContent'>
+                <div>{series.name}</div> 
+                <div>{getGenres(series.genre_ids)}</div>
+                <div>First Air Date: {series.first_air_date}</div>
+                <div>{series.overview}</div>
+              </div>
+            </div>
+          )
+        })
+      )
+    }
+  }
 
   useEffect(() => {
-    setCurrentPage(tvList.page)
+    setLoading(true)
+    getTVList()
   }, [])
 
   return (
     <div className='App'>
       <Header />
-      <div>
-        {generateTVSeries}
-      </div>
-      <Pagination currentPage={currentPage} setCurrentPage={setPagination} lastPage={tvList.total_pages}/>
+      {
+        loading ? 
+        <Loading/> :
+        <div>
+          <div>
+            {generateTVShows()}
+          </div>
+          <Pagination currentPage={currentPage} setCurrentPage={setPagination} lastPage={lastPage}/>
+        </div>
+      }
     </div>
   )
 }
